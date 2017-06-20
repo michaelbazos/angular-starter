7bf228776d453833df27246ac61520d7f0700dbf
Michael Bazos <micabazos@gmail.com>
2017-06-20 16:27:26 +0200

    feat: add compodoc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const AppRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent }
 ];
